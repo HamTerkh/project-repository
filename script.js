c8af0ea88c261a08913dfb46b3d7be1884ef39f6
Takeshi Kowach <terx84@mail.ru>
2025-11-20 20:51:07 +0300

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var _myVar = 10;
+let snake_case = 10;
+const width = 11;
+
+var personAge = 20;
+console.log("personAge", personAge);
+
+const logged = () => {
+  console.log(div.textContent);
+};
+console.log("commits");
